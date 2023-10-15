0a80e8ee4019e24cacc06ad994ebf1610d6d0e44
Liam Collod <lcollod@gmail.com>
2023-10-15 11:02:23 +0200

    feat(io): add extract_zip function

--- python/rezbuild_utils/_io.py
+++ python/rezbuild_utils/_io.py
@@ -2,6 +2,7 @@ import distutils.dir_util
 import logging
 import os
 import shutil
+import zipfile
 from pathlib import Path
 
 
@@ -33,3 +34,39 @@ def copy_build_files(files: list[Path]):
                 str(file),
                 str(target_dir / file.name),
             )
+
+
+def extract_zip(zip_path: Path, reference_file_name: str):
+    """
+    Exract the given zip archive at its root, and move its content up at the root too.
+
+    As an archive can have an arbitrary number of folder nested before finding the actual
+    content, you can provide a reference_file_name to find where the content lives ::
+
+        myarchive.zip
+        myarchive/
+            subfolder/
+                bin/
+                license.txt
+                app.exe
+
+    In the above example, you can use ``app.exe`` as reference file name.
+
+    Args:
+        zip_path: path to an existing zip file on disk.
+        reference_file_name: file name with extension or glob patterns.
+
+    Returns:
+        root direcvtory the file can be found at
+    """
+    extract_root = zip_path.parent
+    with zipfile.ZipFile(zip_path, "r") as zip_file:
+        zip_file.extractall(extract_root)
+    zip_path.unlink()
+
+    reference_file = list(extract_root.glob(f"**/{reference_file_name}"))[0]
+    for file in reference_file.parent.glob("*"):
+        file.rename(extract_root / file.name)
+    # this will raise if not everything was moved
+    reference_file.parent.rmdir()
+    return extract_root
