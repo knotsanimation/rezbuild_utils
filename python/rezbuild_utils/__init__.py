a6dae9074892980fada40a25b4e16a35ef3dfba9
Liam Collod <lcollod@gmail.com>
2023-12-17 12:23:59 +0100

    feat(io): add copytree_to_build

--- python/rezbuild_utils/__init__.py
+++ python/rezbuild_utils/__init__.py
@@ -4,6 +4,7 @@ from pythonning.filesystem import move_directory_content
 from pythonning.web import download_file
 from ._io import copy_build_files
 from ._io import set_installed_path_read_only
+from ._io import copytree_to_build
 from ._download import download_and_install_build
 from ._package import preserve_build_attributes
 from ._pip import install_pip_package
--- python/rezbuild_utils/_io.py
+++ python/rezbuild_utils/_io.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from typing import List
 
 from pythonning.filesystem import set_path_read_only
+from pythonning.filesystem import copytree
+from pythonning.progress import ProgressBar
 
 
 LOGGER = logging.getLogger(__name__)
@@ -35,6 +37,35 @@ def copy_build_files(files: List[Path]):
                 file,
                 target_dir / file.name,
             )
+
+
+def copytree_to_build(src_dir: Path, show_progress: bool = True):
+    """
+    Recursively copy the src_dir to the rez build directory.
+
+    Args:
+        src_dir: filesystem path to an existing directory
+        show_progress: True to display a progress bar in the console.
+    """
+    target_dir = Path(os.environ["REZ_BUILD_INSTALL_PATH"])
+
+    progress = None
+    if show_progress:
+        progress = ProgressBar(
+            prefix="copying {bar_max} paths",
+            suffix="[{bar_index:<2n}/{bar_max}] elapsed {elapsed_time:.2f}s",
+        )
+
+    def _callback(_path: Path, _index: int, _total: int):
+        progress.set_progress(_index, new_maximum=_total)
+
+    LOGGER.debug(f"copying {src_dir} to {target_dir} ...")
+    if progress:
+        progress.start()
+        copytree(src_dir, target_dir, callback=_callback)
+        progress.end()
+    else:
+        shutil.copytree(src_dir, target_dir)
 
 
 def set_installed_path_read_only() -> list[Path]:
